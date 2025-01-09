9e57fd43c43c3380d9cffccf2805c314356c7e84
ooTruffle <Truffle_YT@hotmail.com>
2025-01-10 12:42:43 +1300

    Update what shows

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const axios = require('axios');
 const fs = require('fs');
 require("dotenv").config();
 
-// Create a new Discord client
+
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-// File path for the linked accounts JSON file
+
 const linkedAccountsFilePath = './linkedAccounts.json';
 
-// Function to load linked accounts from JSON file
+
 function loadLinkedAccounts() {
     if (!fs.existsSync(linkedAccountsFilePath)) {
         return {};
@@ -30,18 +30,17 @@ function loadLinkedAccounts() {
     return JSON.parse(data);
 }
 
-// Load linked accounts on startup
 let linkedAccounts = loadLinkedAccounts();
 
-// Function to save linked accounts to JSON file
+
 function saveLinkedAccounts() {
     fs.writeFileSync(linkedAccountsFilePath, JSON.stringify(linkedAccounts, null, 2));
 }
 
-// In-memory cache for Hypixel API responses
+
 const cache = {};
 
-// Function to cache player data
+
 async function cachePlayerData(playerName) {
     const cacheKey = `playerData-${playerName}`;
     const now = Date.now();
@@ -56,7 +55,7 @@ async function cachePlayerData(playerName) {
         const response = await axios.get(url);
 
         if (response.data.cause === 'You have already looked up this name recently') {
-            return 'You have already looked up this name recently. Please wait a while before trying again.';
+            return { error: 'You have already looked up this name recently. Please wait a while before trying again.' };
         }
 
         const playerData = response.data.player;
@@ -70,11 +69,11 @@ async function cachePlayerData(playerName) {
         return playerData;
     } catch (error) {
         console.error(error);
-        throw new Error(`Failed to fetch data for player ${playerName}`);
+        return { error: `Failed to fetch data for player ${playerName}, This could be that the name has been looked up recently` };
     }
 }
 
-// Function to get the UUID of a Minecraft player
+
 async function getPlayerUUID(playerName) {
     const playerData = await cachePlayerData(playerName);
     if (playerData && playerData.uuid) {
@@ -84,9 +83,15 @@ async function getPlayerUUID(playerName) {
     }
 }
 
-// Function to get Lucky Block stats from Hypixel API
+
 async function getLuckyBlockStats(gameType, playerName) {
     const playerData = await cachePlayerData(playerName);
+
+    // Check if there was an error
+    if (playerData.error) {
+        return playerData.error;
+    }
+
     if (!playerData || !playerData.stats || !playerData.stats.Bedwars) {
         return `Player ${playerName} not found or no Bedwars stats available.`;
     }
@@ -106,6 +111,10 @@ async function getLuckyBlockStats(gameType, playerName) {
         "Mob Kills": playerData.stats.Bedwars[`${modePrefix}_entity_attack_kills_bedwars`] || 0,
         "Creeper Kills": playerData.stats.Bedwars[`${modePrefix}_entity_explosion_kills_bedwars`] || 0,
         "Thorn Kills": playerData.stats.Bedwars[`${modePrefix}_thorns_kills_bedwars`] || 0,
+        "Fire Kills": playerData.stats.Bedwars[`${modePrefix}_fire_kills_bedwars`] || 0,
+        "Firetick Kills": playerData.stats.Bedwars[`${modePrefix}_fire_tick_kills_bedwars`] || 0,
+        "Projectile Kills": playerData.stats.Bedwars[`${modePrefix}_projectile_kills_bedwars`] || 0,
+        "Magic Kills": playerData.stats.Bedwars[`${modePrefix}_magic_kills_bedwars`] || 0,
         "Deaths": playerData.stats.Bedwars[`${modePrefix}_deaths_bedwars`] || 0,
         "Void Deaths": playerData.stats.Bedwars[`${modePrefix}_void_deaths_bedwars`] || 0,
         "Mob Deaths": playerData.stats.Bedwars[`${modePrefix}_entity_attack_deaths_bedwars`] || 0,
@@ -117,12 +126,22 @@ async function getLuckyBlockStats(gameType, playerName) {
         "Fall Damage Final Kills": playerData.stats.Bedwars[`${modePrefix}_fall_final_kills_bedwars`] || 0,
         "Lava Final Kills": playerData.stats.Bedwars[`${modePrefix}_lava_final_kills_bedwars`] || 0,
         "Thorn Final Kills": playerData.stats.Bedwars[`${modePrefix}_thorns_final_kills_bedwars`] || 0,
+        "Firetick Final Kills": playerData.stats.Bedwars[`${modePrefix}_fire_tick_final_kills_bedwars`] || 0,
+        "Magic Final Kills": playerData.stats.Bedwars[`${modePrefix}_magic_final_kills_bedwars`] || 0,
+        "Creeper Final Kills": playerData.stats.Bedwars[`${modePrefix}_entity_explosion_final_kills_bedwars`] || 0,
         "Final Deaths": playerData.stats.Bedwars[`${modePrefix}_final_deaths_bedwars`] || 0,
         "Void Final Deaths": playerData.stats.Bedwars[`${modePrefix}_void_final_deaths_bedwars`] || 0,
         "Mob Final Deaths": playerData.stats.Bedwars[`${modePrefix}_entity_attack_final_deaths_bedwars`] || 0,
         "Creeper Final Deaths": playerData.stats.Bedwars[`${modePrefix}_entity_explosion_final_deaths_bedwars`] || 0,
+        "Fall Damage Final Deaths": playerData.stats.Bedwars[`${modePrefix}_fall_final_deaths_bedwars`] || 0,
         "Lava Final Deaths": playerData.stats.Bedwars[`${modePrefix}_lava_final_deaths_bedwars`] || 0,
         "Thorn Final Deaths": playerData.stats.Bedwars[`${modePrefix}_thorns_final_deaths_bedwars`] || 0,
+        "Magic Final Deaths": playerData.stats.Bedwars[`${modePrefix}_magic_final_deaths_bedwars`] || 0,
+        "Suffocation Final Deaths": playerData.stats.Bedwars[`${modePrefix}_suffocation_final_deaths_bedwars`] || 0,
+        "Wither Final Deaths": playerData.stats.Bedwars[`${modePrefix}_wither_final_deaths_bedwars`] || 0,
+        "Fire Tick Final Deaths": playerData.stats.Bedwars[`${modePrefix}_fire_tick_final_deaths_bedwars`] || 0,
+        "Block Explosion Final Deaths": playerData.stats.Bedwars[`${modePrefix}_block_explosion_final_deaths_bedwars`] || 0,
+        "Projectile Final Deaths": playerData.stats.Bedwars[`${modePrefix}_projectile_final_deaths_bedwars`] || 0,
         "Placeable beds collected": playerData.stats.Bedwars[`${modePrefix}_bed_resources_collected_bedwars`] || 0,
         "Magic Deaths": playerData.stats.Bedwars[`${modePrefix}_magic_deaths_bedwars`] || 0,
         "Block Explosion Deaths": playerData.stats.Bedwars[`${modePrefix}_block_explosion_deaths_bedwars`] || 0,
@@ -133,7 +152,7 @@ async function getLuckyBlockStats(gameType, playerName) {
     return luckyBlockStats;
 }
 
-// Function to get simplified Lucky Block stats from Hypixel API
+
 async function getSimpleLuckyBlockStats(gameType, playerName) {
     const playerData = await cachePlayerData(playerName);
     if (!playerData || !playerData.stats || !playerData.stats.Bedwars) {
@@ -158,7 +177,7 @@ async function getSimpleLuckyBlockStats(gameType, playerName) {
     return simpleLuckyBlockStats;
 }
 
-// Function to calculate ratios
+
 function calculateRatios(stats) {
     const winLossRatio = stats.Wins / (stats.Loss || 1);
     const killDeathRatio = stats.Kills / (stats.Deaths || 1);
@@ -175,24 +194,16 @@ function calculateRatios(stats) {
     };
 }
 
-// Function to format stats as a string
-function formatStats(gameMode, ign, stats) {
-    let formattedStats = `Stats in Lucky Block ${gameMode} for ${ign}:\n\n`;
-    for (const [key, value] of Object.entries(stats)) {
-        formattedStats += `${key}: ${value}\n`;
-    }
-    return formattedStats.trim();
-}
 
 
-// Event listener when the bot is ready
+
 client.once('ready', async () => {
     console.log('Bot is online!');
     try {
-        // Set initial status
+        
         client.user.setActivity("People Open Blocks", { type: ActivityType.Watching });
 
-        // Define an array of statuses
+       
         const statuses = [
             { name: "People getting Rouletted", type: ActivityType.Watching },
             { name: "People Open Blocks", type: ActivityType.Watching },
@@ -200,18 +211,18 @@ client.once('ready', async () => {
             
         ];
 
-        // Set interval to change status every 10 minutes (600000 ms)
+        
         let statusIndex = 0;
         setInterval(() => {
             statusIndex = (statusIndex + 1) % statuses.length;
             client.user.setActivity(statuses[statusIndex].name, { type: statuses[statusIndex].type });
-        }, 300000); // 10 minutes in milliseconds
+        }, 300000); 
     } catch (error) {
         console.error("Error during bot initialization:", error);
     }
 });
 
-// Event listener for slash commands
+
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand())
         return;
@@ -237,18 +248,84 @@ client.on('interactionCreate', async interaction => {
             return;
         }
     
-        // Fetch the UUID of the player
+        if (stats.error) {
+            await interaction.reply(stats.error);
+            return;
+        }
+
         const uuid = await getPlayerUUID(playerName);
     
-        // Create an embed message
         const statsEmbed = new EmbedBuilder()
-            .setColor(0x0099ff)
+            .setColor(`#e4ff00`)
             .setTitle(`Stats in Lucky Block ${gameType} for ${playerName}`)
-            .setDescription(Object.entries(stats).map(([key, value]) => `${key}: ${value}`).join('\n'))
+            .addFields(
+                { name: 'Basic Stats', value: `
+                    Winstreak: ${stats.Winstreak}
+                    Games Played: ${stats["Games Played"]}
+                    Wins: ${stats.Wins}
+                    Loss: ${stats.Loss}
+                    Beds Broken: ${stats["Beds Broken"]}
+                    Beds Lost: ${stats["Beds Lost"]}
+                    Placeable beds collected: ${stats["Placeable beds collected"]}
+                ` , inline: true},
+                { name: 'Kill Stats', value: `
+                    Kills: ${stats.Kills}
+                    Fall Damage Kills: ${stats["Fall Damage Kills"]}
+                    Void Kills: ${stats["Void Kills"]}
+                    Mob Kills: ${stats["Mob Kills"]}
+                    Creeper Kills: ${stats["Creeper Kills"]}
+                    Thorn Kills: ${stats["Thorn Kills"]}
+                    Fire Kills: ${stats["Fire Kills"]}
+                    Firetick Kills: ${stats["Firetick Kills"]}
+                    Projectile Kills: ${stats["Projectile Kills"]}
+                    Magic Kills: ${stats["Magic Kills"]}
+                ` , inline: true},
+                { name: 'Death Stats', value: `
+                    Deaths: ${stats.Deaths}
+                    Void Deaths: ${stats["Void Deaths"]}
+                    Mob Deaths: ${stats["Mob Deaths"]}
+                    Fall Damage Deaths: ${stats["Fall Damage Deaths"]}
+                    Thorn Deaths: ${stats["Thorn Deaths"]}
+                    Magic Deaths: ${stats["Magic Deaths"]}
+                    Falling Block Deaths: ${stats["Falling Block Deaths"]}
+                `, inline: true},
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                { name: 'Final Kill Stats', value: `
+                    Final Kills: ${stats["Final Kills"]}
+                    Void Final Kills: ${stats["Void Final Kills"]}
+                    Mob Final Kills: ${stats["Mob Final Kills"]}
+                    Fall Damage Final Kills: ${stats["Fall Damage Final Kills"]}
+                    Lava Final Kills: ${stats["Lava Final Kills"]}
+                    Thorn Final Kills: ${stats["Thorn Final Kills"]}
+                    Firetick Final Kills: ${stats["Firetick Final Kills"]}
+                    Magic Final Kills: ${stats["Magic Final Kills"]}
+                    Creeper Final Kills: ${stats["Creeper Final Kills"]}
+                `, inline: true},
+                { name: 'Final Death Stats', value: `
+                    Final Deaths: ${stats["Final Deaths"]}
+                    Void Final Deaths: ${stats["Void Final Deaths"]}
+                    Mob Final Deaths: ${stats["Mob Final Deaths"]}
+                    Creeper Final Deaths: ${stats["Creeper Final Deaths"]}
+                    Fall Damage Final Deaths: ${stats["Fall Damage Final Deaths"]}
+                    Lava Final Deaths: ${stats["Lava Final Deaths"]}
+                    Thorn Final Deaths: ${stats["Thorn Final Deaths"]}
+                    Falling Block Final Deaths: ${stats["Falling Block Final Deaths"]}
+                    Magic Final Deaths: ${stats["Magic Final Deaths"]}
+                    Suffocation Final Deaths: ${stats["Suffocation Final Deaths"]}
+                    Wither Final Deaths: ${stats["Wither Final Deaths"]}
+                    Fire Tick Final Deaths: ${stats["Fire Tick Final Deaths"]}
+                    Block Explosion Final Deaths: ${stats["Block Explosion Final Deaths"]}
+                    Projectile Final Deaths: ${stats["Projectile Final Deaths"]}
+                `, inline: true}
+            )
             .setTimestamp()
             .setFooter({ text: `${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
             .setThumbnail(`https://minotar.net/helm/${uuid}.png`);
-    
+
         await interaction.reply({ embeds: [statsEmbed] });
     } else if (commandName === 'link') {
         const minecraftIGN = options.getString('minecraft_ign');
@@ -275,7 +352,6 @@ client.on('interactionCreate', async interaction => {
         }
 
         try {
-            // Fetch the UUID of the player
             const uuid = await getPlayerUUID(playerName);
 
             const stats = await getLuckyBlockStats(gameType, playerName);
@@ -285,9 +361,101 @@ client.on('interactionCreate', async interaction => {
             }
 
             const ratios = calculateRatios(stats);
-            // Create an embed message
             const ratioEmbed = new EmbedBuilder()
-                .setColor(0x0099ff)
+                .setColor(`#e4ff00`)
+                .setTitle(`Ratios for ${playerName} in Lucky Block ${gameType}`)
+                .addFields(
+                    { name: 'Win/Loss', value: ratios.winLossRatio.toFixed(2), inline: true },
+                    { name: 'Kill/Death', value: ratios.killDeathRatio.toFixed(2), inline: true },
+                    { name: 'Final Kill/Final Death', value: ratios.finalKillDeathRatio.toFixed(2), inline: true },
+                    { name: 'Void Kill/Death', value: ratios.VoidKillRatio.toFixed(2), inline: true },
+                    { name: 'Void Final Kill/Death', value: ratios.VoidFinalKillRatio.toFixed(2), inline: true }
+                )
+                .setTimestamp()
+                .setFooter({ text: `${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                .setThumbnail(`https://minotar.net/helm/${uuid}.png`);
+
+            await interaction.reply({ embeds: [ratioEmbed] });
+        } catch (error) {
+            await interaction.reply(`Failed to fetch player UUID for ${playerName}.`);
+        }
+    } else if (commandName === 'simplestats') {
+        const gameType = options.getString('gamemode');
+        let playerName = options.getString('player');
+
+        if (!playerName) {
+            if (linkedAccounts[user.id]) {
+                playerName = linkedAccounts[user.id];
+            } else {
+                await interaction.reply('No player specified and no linked account found.');
+                return;
+            }
+        }
+
+        const stats = await getSimpleLuckyBlockStats(gameType, playerName);
+        if (typeof stats === 'string') {
+            await interaction.reply(stats);
+            return;
+        }
+
+        const uuid = await getPlayerUUID(playerName);
+
+        const simpleStatsEmbed = new EmbedBuilder()
+            .setColor(`#e4ff00`)
+            .setTitle(`Simple stats for ${playerName} in Lucky Block ${gameType}`)
+            .addFields(
+                { name: 'Winstreak', value: stats.Winstreak.toString(), inline: true },
+                { name: 'Games Played', value: stats["Games Played"].toString(), inline: true },
+                { name: 'Wins', value: stats.Wins.toString(), inline: true },
+                { name: 'Loss', value: stats.Loss.toString(), inline: true },
+                { name: 'Beds Broken', value: stats["Beds Broken"].toString(), inline: true },
+                { name: 'Beds Lost', value: stats["Beds Lost"].toString(), inline: true },
+                { name: 'Kills', value: stats.Kills.toString(), inline: true },
+                { name: 'Deaths', value: stats.Deaths.toString(), inline: true },
+                { name: 'Final Kills', value: stats["Final Kills"].toString(), inline: true },
+                { name: 'Final Deaths', value: stats["Final Deaths"].toString(), inline: true }
+            )
+            .setTimestamp()
+            .setFooter({ text: `${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+            .setThumbnail(`https://minotar.net/helm/${uuid}.png`);
+
+        await interaction.reply({ embeds: [simpleStatsEmbed] });
+    } else if (commandName === 'link') {
+        const minecraftIGN = options.getString('minecraft_ign');
+        const verifiedUsername = await verifyMinecraftUsername(minecraftIGN);
+
+        if (verifiedUsername) {
+            linkedAccounts[user.id] = verifiedUsername;
+            saveLinkedAccounts();
+            await interaction.reply(`Your Hypixel account ${verifiedUsername} has been successfully linked.`);
+        } else {
+            await interaction.reply('Could not verify the Minecraft username. Please make sure it is correct.');
+        }
+    } else if (commandName === 'ratios') {
+        const gameType = options.getString('gamemode');
+        let playerName = options.getString('player');
+
+        if (!playerName) {
+            if (linkedAccounts[user.id]) {
+                playerName = linkedAccounts[user.id];
+            } else {
+                await interaction.reply('No player specified and no linked account found.');
+                return;
+            }
+        }
+
+        try {
+            const uuid = await getPlayerUUID(playerName);
+
+            const stats = await getLuckyBlockStats(gameType, playerName);
+            if (typeof stats === 'string') {
+                await interaction.reply(stats);
+                return;
+            }
+
+            const ratios = calculateRatios(stats);
+            const ratioEmbed = new EmbedBuilder()
+                .setColor(`#e4ff00`)
                 .setTitle(`Ratios for ${playerName} in Lucky Block ${gameType}`)
                 .addFields(
                     { name: 'Win/Loss', value: ratios.winLossRatio.toFixed(2), inline: true },
@@ -323,12 +491,12 @@ if (typeof stats === 'string') {
     return;
 }
 
-// Fetch the UUID of the player
+
 const uuid = await getPlayerUUID(playerName);
 
-// Create an embed message
+
 const simpleStatsEmbed = new EmbedBuilder()
-    .setColor(0x0099ff)
+    .setColor(`#e4ff00`)
     .setTitle(`Simple stats for ${playerName} in Lucky Block ${gameType}`)
     .addFields(
         { name: 'Winstreak', value: stats.Winstreak.toString(), inline: true },
@@ -366,9 +534,7 @@ await interaction.reply({ embeds: [simpleStatsEmbed] });
     
 
         await interaction.reply({ embeds: [info], components: [row]  });
-    }
-
+    } 
 });
 
-// Log in to Discord
 client.login(process.env.DISCORD_BOT_TOKEN);
